998e41b6ea9960b27d1d6f3ffc8901b7fb0a8c39
Noah Rubin <noahgidon@gmail.com>
2018-07-16 10:06:56 -0500

    Initial Commit

---
+++ changeLockScreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  changeLockScreen
+//
+//  Created by Noah Rubin on 7/16/18.
+//  Copyright © 2018 Noah Rubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
